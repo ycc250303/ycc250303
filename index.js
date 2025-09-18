238cc2e03cbe669665f693340ef3433e79dc6221
ycc250303 <2351279@tongji.edu.cn>
2025-09-18 10:54:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,22 +11,33 @@ function generateProgressBar() {
   return `[ ${progressBar} ]`
 }
 
-const readme = `\
-<h1>
-  👋 Hi there my No.
-  <img src='https://visitor-badge.laobi.icu/badge?page_id=ycc250303.ycc250303'/>
-  visitor!
-</h1>
+const readme = `<h1>👋 Hi there, I'm ycc250303!</h1>
 
 > [!TIP]
 > ⏳ Year Progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 >
 > ⏰ Updated on ${new Date().toUTCString()}
 
-<div align='center'>
-  <img src='https://github-readme-stats.vercel.app/api?username=ycc250303&show_icons=true&count_private=true&cache_seconds=1800' height='190'/>
-  <img src='https://github-readme-stats.vercel.app/api/top-langs/?username=ycc250303&layout=compact&cache_seconds=1800' height='190'/>
-</div>
-`
+### 👨‍💻 About Me
 
+### 📊 My GitHub
+
+<div align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=ycc250303&show_icons=true&count_private=true&rank_icon=github&line_height=27&custom_title=GitHub%20Stats&show=reviews,discussions_started,discussions_answered,prs_merged,prs_merged_percentage&cache_seconds=1800" style="height: 360px"/>
+  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ycc250303&layout=compact&hide=jupyter%20notebook&langs_count=20&cache_seconds=1800" style="height: 360px"/>
+</div>
+
+<div align="center">
+  <img src="https://visitor-badge.laobi.icu/badge?page_id=ycc250303.ycc250303"/>
+</div>
+
+### ⏱️ My WakaTime
+
+<div align="center">
+  <img src="https://github-readme-stats.vercel.app/api/wakatime?username=ycc250303&layout=compact&cache_seconds=1800"/>
+</div>
+
+---
+
+`
 console.log(readme)
