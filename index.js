30aef5838cd87efd8993f3f36a2f9bca48ad817b
ycc250303 <2351279@tongji.edu.cn>
2025-09-22 15:55:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ const readme = `<h1>👋 Hi there, I'm ycc250303!</h1>
 ### 📊 My GitHub
 
 <div align="center">
-  <img src="https://github-readme-stats.vercel.app/api?username=ycc250303&show_icons=true&count_private=true&rank_icon=github&line_height=27&custom_title=GitHub%20Stats&show=reviews,discussions_started,discussions_answered,prs_merged,prs_merged_percentage&cache_seconds=1800" style="height: 240px"/>
-  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ycc250303&layout=compact&hide=jupyter%20notebook&langs_count=20&cache_seconds=1800" style="height: 240px"/>
+  <img src="https://github-readme-stats.vercel.app/api?username=ycc250303&show_icons=true&count_private=true&rank_icon=github&line_height=27&custom_title=GitHub%20Stats&show=reviews,discussions_started,discussions_answered,prs_merged,prs_merged_percentage&cache_seconds=1800" style="height: 300px"/>
+  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ycc250303&layout=compact&hide=jupyter%20notebook&langs_count=20&cache_seconds=1800" style="height: 300px"/>
 </div>
 
 
