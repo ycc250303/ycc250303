29a7477d4d6a3c68ec33a2c8b38906a4081efcc8
ycc250303 <2351279@tongji.edu.cn>
2026-01-10 19:38:27 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
@@ -19,7 +20,7 @@ const readme = `<h1>👋 Hi there, I'm ycc250303!</h1>
 > ⏰ Updated on ${new Date().toUTCString()}
 
 <div align="center">
-  <img src="https://visitor-badge.laobi.icu/badge?page_id=ycc250303.ycc250303"/>
+  <img src="https://visitor-badge.laobi.icu/badge?page_id=ycc250303.ycc250303" alt="Visitors"/>
 </div>
 
 ### 👨‍💻 About Me
@@ -32,13 +33,11 @@ const readme = `<h1>👋 Hi there, I'm ycc250303!</h1>
 ### 📊 My GitHub
 
 <div align="center">
-  <img src="https://github-readme-stats.vercel.app/api?username=ycc250303&show_icons=true&count_private=true&rank_icon=github&line_height=27&custom_title=GitHub%20Stats&show=reviews,discussions_started,discussions_answered,prs_merged,prs_merged_percentage&cache_seconds=1800" style="height: 300px"/>
-  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ycc250303&layout=compact&hide=jupyter%20notebook&langs_count=20&cache_seconds=1800" style="height: 300px"/>
+  <img src="https://github-readme-stats.vercel.app/api?username=ycc250303&show_icons=true&count_private=true&theme=radical&hide_border=false" height="192" alt="GitHub Stats"/>
+  <img src="https://github-readme-stats.vercel.app/api/top-langs?username=ycc250303&layout=compact&theme=radical&hide_border=false&hide=jupyter%20notebook&langs_count=10" height="192" alt="Top Languages"/>
 </div>
 
-
-
 ---
-
 `
-console.log(readme)
+fs.writeFileSync('README.md', readme)
+console.log('README.md updated successfully!')
